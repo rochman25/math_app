cbafb781be398e7a5bbf5389254e1fc6e6d749b4
Zaenurrochman <zaenur.rochman98@gmail.com>
2021-08-21 15:44:27 +0700

    refactor validation argument

--- src/FigureCalculator.js
+++ src/FigureCalculator.js
@@ -3,47 +3,43 @@ class FigureCalculator {
         this._mathBasic = mathBasic;
     }
     
-    _validateArgs(args) {
-        if(args.length !== 2){
-            throw new Error('fungsi hanya menerima dua parameter');
+    _validateArgs(args, expectedArgsCount) {
+        if(args.length !== expectedArgsCount){
+            throw new Error(`fungsi hanya menerima ${expectedArgsCount} parameter`);
         }
 
         const [a, b] = args;
 
-        if(typeof a !== 'number' || typeof b !== 'number') {
-            throw new Error('fungsi hanya menerima parameter number');
-        }
+        args.forEach((arg) => {
+            if (typeof arg !== 'number') {
+              throw new Error('fungsi hanya menerima parameter number');
+            }
+        });
+
         return args;
     }
 
     calculateRectangelePermeter(...args) {
-        const [length, width] = this._validateArgs(args);
+        const [length, width] = this._validateArgs(args, 2);
         // formula: (2 * (length + width))
         return this._mathBasic.multiply(2, this._mathBasic.add(length, width));
     }
     
     calculateRectangleArea(...args) {
-        const [length, width] = this._validateArgs(args);
+        const [length, width] = this._validateArgs(args, 2);
         // formula: (length * width)
         return this._mathBasic.multiply(length, width);
     }
 
     calculateTrianglePerimeter(...args) {
-        if(args.length !== 3){
-            throw new Error('fungsi hanya menerima tiga parameter');
-        }
-
-        const [a, b, c] = args;
-
-        if(typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number') {
-            throw new Error('fungsi hanya menerima parameter number');
-        }
-
+        const [a, b, c] = this._validateArgs(args, 3);
+        // formula: (a + b) + c
         return this._mathBasic.add(this._mathBasic.add(a,b),c);
     }
 
     calculateTriangleArea(...args) {
-        const [base, height] = this._validateArgs(args);
+        const [base, height] = this._validateArgs(args, 2);
+        // formula: (base * height) / 2
         return this._mathBasic.divide(this._mathBasic.multiply(base, height),2);
     }
 
